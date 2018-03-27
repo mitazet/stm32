300f02a23a1bc32a177bf45a8d851346e36ff006
mitazet <mitazet@gmail.com>
2018-03-27 21:58:04 +0900

    add rtos.h

---
+++ v7/Main/rtos/rtos.h
@@ -0,0 +1,15 @@
+#ifndef __RTOS_H__
+
+// System call
+rtos_thread_id t RtosRun(rtos_func_t func, char* name, int stacksize, int argc, char* argv[]);
+void RtosTerminate(void);
+
+// Library function
+void RtosStart(rtos_func_t func, char* name, int stacksize, int argc, char* argv[]);
+void RtosSysdown(void);
+void RtosSyscall(rtos_syscall_type_t type, rtos_syscall_param_t* param);
+
+// User Thread
+int test08_1_main(int argc, char* argv[]);
+
+#endif
