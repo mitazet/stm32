2f636b48514fe7c6a274f2679da8fe155b6d5474
mitazet <mitazet@gmail.com>
2018-07-16 14:51:48 +0900

    add flash driver headder

---
+++ v10/Boot/include/flash_driver.h
@@ -0,0 +1,20 @@
+#ifndef __FLASH_DRIVER__
+#define __FLASH_DRIVER__
+
+#include "stm32f303x8.h"
+
+#define FLASH_PAGE_SIZE_BYTE 2048
+
+typedef enum{
+    FLASH_RESULT_NG,
+    FLASH_RESULT_OK,
+}flash_result_t;
+
+extern void FlashCreate(FLASH_TypeDef* flash_address);
+extern void FlashInit(void);
+extern uint8_t FlashRead(uint8_t* address);
+extern flash_result_t FlashWrite(uint16_t* address, uint16_t data);
+extern flash_result_t FlashPageErase(uint8_t* address);
+extern flash_result_t FlashMassErase(void);
+
+#endif
